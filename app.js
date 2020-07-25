c074346e54cd9e56f25a9b89a82431007f7ff848
Nabeel <35260705+NabilRafiq@users.noreply.github.com>
2020-07-25 15:40:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var list=document.getElementById('list');
+function additem(){
+    var todo=document.getElementById('todotask');
+    var li=document.createElement('li');
+    var litext=document.createTextNode(todo.value);
+    li.appendChild(litext);
+    var delbtn=document.createElement("button");
+    var deltext=document.createTextNode('Delete');
+    delbtn.classList.add('btn');
+    delbtn.classList.add('btn-primary');
+    delbtn.classList.add('ml-1');
+    delbtn.classList.add('mt-1');
+    delbtn.setAttribute("onclick","delitem(this)");
+    delbtn.appendChild(deltext);
+    var editbtn=document.createElement('button');
+    var edittext=document.createTextNode('Edit');
+    editbtn.classList.add('btn');
+    editbtn.classList.add('btn-primary');
+    editbtn.classList.add('ml-1');
+    editbtn.classList.add('mt-1');
+    editbtn.setAttribute("onclick","editvalue(this)");
+    editbtn.appendChild(edittext);
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
+    checkbox.classList.add('ml-1');
+    li.appendChild(checkbox);
+    li.appendChild(editbtn);
+    li.appendChild(delbtn);
+    list.appendChild(li);
+    todo.value="";
+}
+function delitem(obj){
+    obj.parentNode.remove();
+}
+function delall(){
+    list.innerHTML="";
+}
+function editvalue(obj){
+obj.parentNode.firstChild.nodeValue=prompt("Enter new value",obj.parentNode.firstChild.nodeValue);
+}
